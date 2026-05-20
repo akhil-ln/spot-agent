97fdc47d144f73d8a311289c7e514e19384e3270
Akhil <analytics@logisticsnow.in>
2026-05-20 23:57:01 +0530

    fix

--- api/index.py
+++ api/index.py
@@ -1,12 +1,6 @@
 import sys
 import os
 
-# Add current dir
-sys.path.append(os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(__file__))
 
-try:
-    # ✅ Vercel (root execution)
-    from api.main import app
-except ModuleNotFoundError:
-    # ✅ Local dev (inside /api)
-    from main import app
+from main import app
